23c154b491cfc60ff3041bc06e0ba7a5c63fa718
kristoffkopp <kristoffer.kopperud@gmail.com>
2017-02-17 13:11:34 +0100

    IntegratingsTest for pointDataArrays translation and rotation

--- ReadVtkTESTtest/IntergrationsTestPoints.cs
+++ ReadVtkTESTtest/IntergrationsTestPoints.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ReadVtkTEST;
+using Kitware.VTK;
 
 namespace ReadVtkTESTtest
 {
 	[TestClass]
 	public class IntergrationsTestPoints
 	{
+		private vtkUnstructuredGrid unstructuredGrid;
+		private VTKPointReader pointReader;
+		private double[,] translation;
+		private double[,] rotationVector;
+		private double[] extremeDisplacement;
+
+		[TestInitialize]
+		public void init()
+		{
+			VTKreader reader = new VTKreader(@"C:\Users\Kristoffer\Dropbox\Dokumenter\Masteroppgave - Spring 2017\TDD vtk-files\TESTTESTTESTTEST.vtu");
+			unstructuredGrid = reader.readFile();
+			pointReader = new VTKPointReader();
+
+			VTKPointDataReader pointDataReader = new VTKPointDataReader();
+			translation = pointDataReader.readTranslation(unstructuredGrid);
+			rotationVector = pointDataReader.readRotationVectors(unstructuredGrid);
+			extremeDisplacement = pointDataReader.ExtremeDisplacement;
+		}
 		[TestMethod]
 		public void Canary()
 		{
@@ -15,18 +34,52 @@ namespace ReadVtkTESTtest
 		[TestMethod]
 		public void VTUTestFileReadNumberOfPointsShouldBeFour()
 		{
-			VTKreader reader = new VTKreader(@"C:\Users\Kristoffer\Dropbox\Dokumenter\Masteroppgave - Spring 2017\TDD vtk-files\TESTTESTTESTTEST.vtu");
-			var unstructuredGrid = reader.readFile();
-
-			VTKPointReader pointReader = new VTKPointReader();
 			var vectorPoints = pointReader.pointReader(unstructuredGrid);
-
 			Assert.AreEqual(4, vectorPoints.Count);
 		}
 		[TestMethod]
-		public void Canary2()
+		public void VTUTestFileReadXCoordinateShouldBeZeroOfFirstPoint()
 		{
-			Assert.IsTrue(true);
+			var vectorPoints = pointReader.pointReader(unstructuredGrid);
+			Assert.AreEqual(0, vectorPoints[0].X); ;
 		}
+		[TestMethod]
+		public void VTUTestFileReadYCoordinateShouldBeOneOfThirdPoint()
+		{
+			var vectorPoints = pointReader.pointReader(unstructuredGrid);
+			Assert.AreEqual(1, vectorPoints[2].Y); ;
+		}
+		[TestMethod]
+		public void VTUTestFileReadZCoordinateShouldBeOneOfForthPoint()
+		{
+			var vectorPoints = pointReader.pointReader(unstructuredGrid);
+			Assert.AreEqual(1, vectorPoints[3].Z); ;
+		}
+		[TestMethod]
+		public void VTUTestFileReadTranslationShouldBeOfLenghtTwelve()
+		{
+			Assert.AreEqual(12, translation.Length);
+		}
+		[TestMethod]
+		public void VTUTestFileReadTranslationShouldBeGivenDoubleAtPointZeroZero()
+		{
+			Assert.AreEqual(0.10000000149, translation[0,0]);
+		}
+		[TestMethod]
+		public void VTUTestFileReadRotaionShouldBeOfLenghtTwelve()
+		{
+			Assert.AreEqual(12, rotationVector.Length);
+		}
+		[TestMethod]
+		public void VTUTestFileReadRotationShouldBeGivenDoubleAtPointZeroTwo()
+		{
+			Assert.AreEqual(0.30000001192, translation[0, 2]);
+		}
+		[TestMethod]
+		public void VTUTestFileReadExtremeDisplacementShouldBeOfLenghtThree()
+		{
+			Assert.AreEqual(3, extremeDisplacement.Length);
+		}
+
 	}
 }
